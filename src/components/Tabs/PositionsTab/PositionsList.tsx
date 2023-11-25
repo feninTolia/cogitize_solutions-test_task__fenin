98868c777906d3c87c0e419f5ab883b308656cef
Anatolii Fenin <fenintolja@gmail.com>
2023-11-25 10:05:26 +0200

    fix

--- src/components/Tabs/PositionsTab/PositionsList.tsx
+++ src/components/Tabs/PositionsTab/PositionsList.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+'use client';
+import React, { useEffect, useState } from 'react';
 import PositionCard from './PositionCard';
 import Button from '@/shared/UI/Buttons/Button';
 import { useDispatch, useSelector } from 'react-redux';
@@ -9,6 +10,12 @@ import {
 } from '@/store/positions/positionsSlice';
 
 const PositionsList = () => {
+  const [isClient, setIsClient] = useState(false);
+
+  useEffect(() => {
+    setIsClient(true);
+  }, []);
+
   const positions = useSelector(
     (state: RootState) => state.positions.positions
   );
@@ -17,38 +24,20 @@ const PositionsList = () => {
   );
   const dispatch = useDispatch();
 
-  let content;
-
-  if (!positions) {
-    content = <div></div>;
-  }
-
-  if (positions) {
-    content = positions?.map((position) => (
-      <PositionCard
-        key={position.id}
-        {...position}
-        onClick={() => dispatch(setSelectedPosition(position))}
-        className={
-          selectedPosition?.id === position.id ? ' box-shadow-border' : ''
-        }
-      />
-    ));
-  }
-
   return (
     <div className=" w-[29.6em] flex flex-col  justify-between pb-[1.6em]">
       <ul className=" flex flex-col gap-[2em] overflow-scroll mb-[1.6em]">
-        {positions?.map((position) => (
-          <PositionCard
-            key={position.id}
-            {...position}
-            onClick={() => dispatch(setSelectedPosition(position))}
-            className={
-              selectedPosition?.id === position.id ? ' box-shadow-border' : ''
-            }
-          />
-        ))}
+        {isClient &&
+          positions?.map((position) => (
+            <PositionCard
+              key={position.id}
+              {...position}
+              onClick={() => dispatch(setSelectedPosition(position))}
+              className={
+                selectedPosition?.id === position.id ? ' box-shadow-border' : ''
+              }
+            />
+          ))}
       </ul>
       <Button
         title="Создать новую должность"
